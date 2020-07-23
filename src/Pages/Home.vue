5181dd92edcf95fcd31911cfdf696e10206f67e8
Nitin Kaware <nitin.kaware1@gmail.com>
2020-07-23 18:14:25 +0530

    Add new sample home page for roueting

---
+++ src/Pages/Home.vue
@@ -0,0 +1,7 @@
+<template>
+<div class="h-screen flex justify-center mt-10">
+    <div>
+        <router-link to="/netlify" class="px-5 py-2 rounded bg-gray-300 hover:bg-gray-400">Visit Netlify</router-link>
+    </div>
+</div>
+</template>
--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from './App.vue'
+import Home from './Pages/Home.vue'
 import Netlify from './Pages/Netlify.vue'
 
 const routes = [
